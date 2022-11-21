facdd938a8bcf76f267fcf8ec00958c0076926e2
Lukasz <dziq243@gamil.com>
2022-11-21 18:44:13 +0100

    clean project

---
+++ src/main/java/springkomis/komis/KomisApplication.java
@@ -0,0 +1,13 @@
+package springkomis.komis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springkomis/komis/KomisApplicationTests.java
@@ -0,0 +1,13 @@
+package springkomis.komis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
